174b9602bd95820505ccc39a199dc47709d239f6
Alan Jackson <a.jackson@case-inc.com>
2016-05-12 18:31:43 -0400

    moved babel-register into prod. update app port

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import Koa from 'koa';
 const app = new Koa();
+const port = process.env.port || 3000;
 
 app.use(ctx => {
   ctx.body = 'Hello World';
 });
 
-app.listen(3000, console.log('Server started on port 3000'));
+app.listen(port, console.log(`Server started on port ${port}`));
 
 export default app;
