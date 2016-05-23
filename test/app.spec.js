150686019962ca11604ab2a710ac95a5f6f13a39
Alan Jackson <ajackson@wework.com>
2016-05-23 15:40:18 -0400

    working database connection with knex

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,13 +1,35 @@
-import app from '../lib/app';
+import app from '../src/app';
 import supertest from 'supertest';
 
-const request = supertest.agent(app.listen());
+const server = supertest.agent(app.listen());
 
-describe('Hello World', function () {
-  it('should say "Hello World"', function (done) {
-    request
+describe('API Index', () => {
+  it('should return API version', (done) => {
+    server
       .get('/')
+      .expect('Content-Type', /json/)
       .expect(200)
-      .expect('Hello World', done);
+      .end((err, res) => {
+        if (err) return done(err);
+        res.status.should.equal(200);
+        res.body.api.should.equal('v1.0');
+        done();
+      });
   });
 });
+
+describe('GET /users', () => {
+  it('should return an array of users as json', (done) => {
+    server
+      .get('/users')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        res.status.should.equal(200);
+        res.body.length.should.be.aboveOrEqual(1);
+        done();
+      });
+  });
+});
+
