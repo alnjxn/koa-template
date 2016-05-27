55f2a33a2fbb6bd4b4c6660dda5da6eb69df402b
Alan Jackson <ajackson@wework.com>
2016-05-27 15:57:17 -0400

    added tests for projects

--- test/app.spec.js
+++ test/app.spec.js
@@ -31,7 +31,7 @@ describe('GET /users', () => {
   });
 });
 
-describe('GET /api/users/:id', () => {
+describe('GET /users/:id', () => {
   it('should return a single user as json if found', (done) => {
     server
       .get('/api/users/1')
@@ -86,3 +86,71 @@ describe('POST /users', () => {
   });
 });
 
+describe('GET /projects', () => {
+  it('should return an array of projects as json', (done) => {
+    server
+      .get('/api/projects')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        res.body.length.should.be.aboveOrEqual(1);
+        done();
+      });
+  });
+});
+
+describe('GET /projects/:id', () => {
+  it('should return a single project as json if found', (done) => {
+    server
+      .get('/api/projects/1')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        res.body.id.should.equal(1);
+        res.body.should.have.ownProperty('name');
+        done();
+      });
+  });
+
+  it('should return a 404 when user not found', (done) => {
+    server
+      .get('/api/projects/0')
+      .expect(404)
+      .end((err) => {
+        if (err) return done(err);
+        done();
+      });
+  });
+});
+
+describe('POST /projects', () => {
+  it('should create a new project and return the new project record', (done) => {
+    server
+      .post('/api/projects')
+      .set('Content-Type', 'application/json')
+      .send({ name: '100 Broadway'})
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        var body = JSON.parse(res.text);
+        body.should.have.ownProperty('id');
+        body.should.have.ownProperty('name');
+        body.name.should.equal('100 Broadway');
+        done();
+      });
+  });
+
+  it('should return a 400 if a name is not provided', (done) => {
+    server
+      .post('/api/projects')
+      .set('Content-Type', 'application/json')
+      .send({ foo: 'bar'})
+      .expect(400)
+      .end((err) => {
+        if (err) return done(err);
+        done();
+      });
+  });
+});
