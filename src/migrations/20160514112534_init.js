d440a94a6bcdf475196b50f3fd86a5df48698a6a
Alan Jackson <ajackson@wework.com>
2016-05-27 15:48:51 -0400

    made name a required field for users and projects

--- src/migrations/20160514112534_init.js
+++ src/migrations/20160514112534_init.js
@@ -2,10 +2,10 @@
 exports.up = (knex) => {
   return knex.schema.createTable('projects', (table) => {
     table.increments('id').primary();
-    table.string('name');
+    table.string('name').notNullable();
   }).createTable('users', (table) => {
     table.increments('id').primary();
-    table.string('name');
+    table.string('name').notNullable();
   }).createTable('users_projects', (table) => {
     table.integer('user_id').references('users.id');
     table.integer('project_id').references('projects.id');
