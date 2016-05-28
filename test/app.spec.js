072001be727dfa02d465cd3b0f2bf7465478a325
Alan Jackson <ajackson@wework.com>
2016-05-28 08:27:38 -0400

    added more tests for users. cleanup test order

--- test/app.spec.js
+++ test/app.spec.js
@@ -18,7 +18,7 @@ describe('API Index', () => {
 });
 
 describe('GET /users', () => {
-  it('should return an array of users as json', (done) => {
+  it('should return an array of users', (done) => {
     server
       .get('/api/users')
       .expect('Content-Type', /json/)
@@ -26,31 +26,6 @@ describe('GET /users', () => {
       .end((err, res) => {
         if (err) return done(err);
         res.body.length.should.be.aboveOrEqual(1);
-        done();
-      });
-  });
-});
-
-describe('GET /users/:id', () => {
-  it('should return a single user as json if found', (done) => {
-    server
-      .get('/api/users/1')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end((err, res) => {
-        if (err) return done(err);
-        res.body.id.should.equal(1);
-        res.body.should.have.ownProperty('name');
-        done();
-      });
-  });
-
-  it('should return a 404 when user not found', (done) => {
-    server
-      .get('/api/users/0')
-      .expect(404)
-      .end((err) => {
-        if (err) return done(err);
         done();
       });
   });
@@ -86,24 +61,10 @@ describe('POST /users', () => {
   });
 });
 
-describe('GET /projects', () => {
-  it('should return an array of projects as json', (done) => {
+describe('GET /users/:id', () => {
+  it('should return a single user if found', (done) => {
     server
-      .get('/api/projects')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end((err, res) => {
-        if (err) return done(err);
-        res.body.length.should.be.aboveOrEqual(1);
-        done();
-      });
-  });
-});
-
-describe('GET /projects/:id', () => {
-  it('should return a single project as json if found', (done) => {
-    server
-      .get('/api/projects/1')
+      .get('/api/users/1')
       .expect('Content-Type', /json/)
       .expect(200)
       .end((err, res) => {
@@ -116,10 +77,38 @@ describe('GET /projects/:id', () => {
 
   it('should return a 404 when user not found', (done) => {
     server
-      .get('/api/projects/0')
+      .get('/api/users/0')
       .expect(404)
       .end((err) => {
         if (err) return done(err);
+        done();
+      });
+  });
+});
+
+describe('GET /users/:id/projects', () => {
+  it('should return an array of users projects', (done) => {
+    server
+      .get('/api/users/1/projects')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        res.body.length.should.be.aboveOrEqual(1);
+        done();
+      });
+  });
+});
+
+describe('GET /projects', () => {
+  it('should return an array of projects', (done) => {
+    server
+      .get('/api/projects')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        res.body.length.should.be.aboveOrEqual(1);
         done();
       });
   });
@@ -154,3 +143,29 @@ describe('POST /projects', () => {
       });
   });
 });
+
+describe('GET /projects/:id', () => {
+  it('should return a single project if found', (done) => {
+    server
+      .get('/api/projects/1')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        res.body.id.should.equal(1);
+        res.body.should.have.ownProperty('name');
+        done();
+      });
+  });
+
+  it('should return a 404 when user not found', (done) => {
+    server
+      .get('/api/projects/0')
+      .expect(404)
+      .end((err) => {
+        if (err) return done(err);
+        done();
+      });
+  });
+});
+
